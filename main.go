853cacf900b585680299733aa21a083a7bfac401
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-07-28 14:05:26 +0900

    fix: standard input

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	const decimal_number = 50
+	fmt.Printf("整数を入力してください=>")
+	stdin := bufio.NewScanner(os.Stdin)
+	stdin.Scan()
+	text := stdin.Text()
+	var decimal_number, err = strconv.Atoi(text)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	if decimal_number == 1 || decimal_number == 0 {
+		fmt.Printf("2進数は、%d", decimal_number)
+		return
+	}
 	const binary = 2
 	var result_list []int
 
@@ -34,13 +50,12 @@ func main() {
 
 	var str = strings.Join(r, "")
 
-	fmt.Printf(str)
+	fmt.Printf("2進数は、%s", str)
 }
 func intSliceToStrSlice(i_list []int) []string {
 	s := make([]string, len(i_list))
 	for n := range i_list {
-		s[n] = string(rune(i_list[n]))
-		fmt.Printf(s[n])
+		s[n] = strconv.Itoa(i_list[n])
 	}
 
 	return s
